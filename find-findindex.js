3abd4ac977e544cdd33be2fc393eacb73c8dc81f
jesus <149453045+jesus-b32@users.noreply.github.com>
2023-11-21 09:25:25 -0800

    completed find/findIndex exercise

--- find-findindex.js
+++ find-findindex.js
@@ -12,7 +12,11 @@ findUserByUsername(users, 'taco') // undefined
 */
 
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function(obj){
+    return obj.username === username;
+  });
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +31,13 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  const user = findUserByUsername(usersArray, username);
+  if(user === undefined) return user;
+  const userIndex = usersArray.findIndex(function(obj){
+    return obj.username === username;
+  });
+  usersArray.splice(userIndex, 1);
+  return user;
+
+}
